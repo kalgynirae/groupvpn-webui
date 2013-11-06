43df71aa2c32a8cf7a606563e3a8ef955efafcb9
Colin Chan <colinchan@lumeh.org>
2013-11-06 16:57:31 -0500

    Use ipaddress to generate IP addresses

--- groupvpn-webui.py
+++ groupvpn-webui.py
@@ -1,23 +1,60 @@
-import itertools
 import json
 import math
 import random
 import string
 
 from flask import Flask, redirect, render_template, request, url_for
-from wtforms import Form, BooleanField, IntegerField, TextField, validators
+import ipaddress
+import wtforms as w
 
 PASSWORD_CHARS = string.ascii_lowercase + string.digits
 PASSWORD_LENGTH = 30
 
-class ConfigurationForm(Form):
-    group_name = TextField("Group name", [validators.DataRequired()])
-    xmpp_host = TextField("XMPP host", [validators.DataRequired()])
-    ip_base = TextField("IP base", [validators.IPAddress()])
-    ip_netmask = TextField("IP netmask", [validators.IPAddress()])
-    machine_count = IntegerField("Number of machines",
-                                 [validators.NumberRange(min=0)])
-    end_to_end_security = BooleanField("End-to-end security")
+class IPNetworkField(w.Field):
+    widget = w.widgets.TextInput()
+
+    def __init__(self, label=None, validators=None, **kwargs):
+        super(IPNetworkField, self).__init__(label, validators, **kwargs)
+
+    def _value(self):
+        if self.raw_data:
+            return self.raw_data[0]
+        elif self.data is not None:
+            return str(self.data)
+        else:
+            return ''
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            try:
+                self.data = ipaddress.ip_network(valuelist[0])
+            except ValueError as e:
+                self.data = None
+                raise ValueError(e)
+
+class ConfigurationForm(w.Form):
+    group_name = w.TextField("Group name", [w.validators.DataRequired()])
+    xmpp_host = w.TextField("XMPP host", [w.validators.DataRequired()])
+    machine_count = w.IntegerField(
+        "Number of machines", [w.validators.NumberRange(min=2)])
+    ip_network = IPNetworkField(
+        "IP network", default=ipaddress.ip_network(u"192.168.0.0/24"),
+        description="Enter the network base address followed by either a "
+                    "netmask or a prefix length.")
+    end_to_end_security = w.BooleanField("End-to-end security")
+
+    def validate(self):
+        return (super(ConfigurationForm, self).validate() and
+                self.validate_enough_addresses())
+
+    def validate_enough_addresses(self):
+        available_addresses = len(list(self.ip_network.data.hosts()))
+        if available_addresses >= self.machine_count.data:
+            return True
+        else:
+            self.ip_network.errors.append("Network only contains {} addresses"
+                                          "".format(available_addresses))
+            return False
 
 app = Flask(__name__)
 app.jinja_env.trim_blocks = True
@@ -27,48 +64,37 @@ app.jinja_env.keep_trailing_newline = True
 def home():
     return redirect(url_for('configurate'))
 
-@app.route('/style.css', methods=['GET'])
-def stylesheet():
-    return redirect(url_for('configurate'))
-
 @app.route('/configurate', methods=['GET', 'POST'])
 def configurate():
     form = ConfigurationForm(request.form)
     if request.method == 'POST' and form.validate():
         configs = make_configs(form.group_name.data, form.xmpp_host.data,
-                               form.ip_base.data, form.ip_netmask.data,
+                               form.ip_network.data,
                                form.machine_count.data,
                                form.end_to_end_security.data)
         return render_template('success.html', form=form, configs=configs)
     return render_template('configuration.html', form=form,
                            post_url=url_for('configurate'))
 
-def make_configs(group_name, xmpp_host, ip_base, ip_netmask, machine_count,
-                 end_to_end_security):
-    username_template = "{}{{:0{}}}".format(group_name,
-                                            int(math.log10(machine_count)) + 1)
-    ips = generate_ips(ip_base, ip_netmask)
+def make_configs(group_name, xmpp_host, ip_network,
+                 machine_count, end_to_end_security):
+    max_digits = int(math.log10(machine_count - 1)) + 1
+    username_template = "{}{{:0{}}}".format(group_name, max_digits)
+    ips = iter(ip_network.hosts())
     configs = []
-    for i in range(machine_count):
-        username = username_template.format(i)
+    for n in range(1, machine_count + 1):
+        username = username_template.format(n)
         password = ''.join(random.choice(PASSWORD_CHARS)
                            for _ in range(PASSWORD_LENGTH))
         data = {
             'xmpp_username': username,
             'xmpp_password': password,
             'xmpp_host': xmpp_host,
-            'ip': next(ips),
-            'ip_netmask': ip_netmask,
+            'ip': str(next(ips)),
         }
         configs.append({'filename': "{}.json".format(username),
                         'data': json.dumps(data, indent=4)})
     return configs
-
-def generate_ips(base, mask):
-    # TODO: Do this better.
-    a, b, c, d = map(int, base.split('.'))
-    for n in itertools.count():
-        yield '{}.{}.{}.{}'.format(a, b, c, d + n)
 
 if __name__ == '__main__':
     app.run(debug=True)
